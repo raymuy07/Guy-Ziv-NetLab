5a388e02274e5fc9d21053c8af2a5f8b4a6f5fab
DESKTOP-D7MOSLV\Guy <guylei@post.bgu.ac.il>
2024-11-25 16:57:23 +0100

    template

--- Lab2/lab2_code_208279620_208660365.ino
+++ Lab2/lab2_code_208279620_208660365.ino
@@ -1,7 +1,8 @@
 // C++ code
 //
 
-
+//set tx pin 
+//set rx pin
 
 void setup()
 {
@@ -9,9 +10,31 @@ void setup()
   Serial.begin(9600); 
   // opens serial port, sets data rate to 9600 bps  //end debug
   
+  ///calculate delta_time depend on bit time
   
 }
 
+//is idle
+
+	//yes idle
+		//check if random_time passed
+
+	//no idle
+		//check if data is set with all added bits
+		//build new data block
+		//if it time to send
+		
+
+		//check last state of clock
+
+
+
+//Rx function
+
+//if it is time to read
+	//enter data to buffer
+	//if counter==5 
+	//check switch case on buffer 
 
 void loop() {
  
