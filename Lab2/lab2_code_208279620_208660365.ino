cd7d85f34594448bc41c87c93c2333bec06fca47
DESKTOP-D7MOSLV\Guy <guylei@post.bgu.ac.il>
2024-11-25 15:04:17 +0100

    arranged libary2

---
+++ Lab2/lab2_code_208279620_208660365.ino
@@ -0,0 +1,19 @@
+// C++ code
+//
+
+
+
+void setup()
+{
+  //for debug
+  Serial.begin(9600); 
+  // opens serial port, sets data rate to 9600 bps  //end debug
+  
+  
+}
+
+
+void loop() {
+ 
+ 
+}
