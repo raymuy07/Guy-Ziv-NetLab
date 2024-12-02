b694e153b7fb64541a446822c5e2b4b3122ce2e6
LAPTOP-LFJH19DP\zivza <zivzaidman1@gmail.com>
2024-12-02 10:42:36 +0200

    working code at bit_time of 20,000 us, 20 milis.

--- Lab2/lab2_code_208279620_208660365.ino
+++ Lab2/lab2_code_208279620_208660365.ino
@@ -1,312 +1,141 @@
-// C++ code
-//
+#define TX_PIN 5                 // Transmission pin
+#define RX_PIN 4                 // Reception pin
+#define BIT_WAIT_TIME 20000    // Bit duration in microseconds (1 second per bit)
+#define DELTA_TIME (BIT_WAIT_TIME / 5) // Sampling granularity
 
-#define Tx_Data_Line 5
-#define Rx_Data_Line 4
-#define BIT_wait_time 50
-
+// Receiver states
 #define IDLE 0
 #define START 1
 #define DATA 2
-#define PARITY 3
-#define STOP 4
+#define STOP 3
 
+// Global variables
+unsigned long rx_last_time = 0;  // Tracks last sampling time
+int rx_state = IDLE;             // Current state of the receiver
+int rx_bit_counter = 0;          // Counter for received data bits
+char rx_frame = 0;               // Stores the received frame
 
-//times
-long current_time;
-long Rx_ref_time , Tx_ref_time;
-long random_wait_time;
-long delta_time;
+unsigned long tx_last_time = 0;  // Tracks last transmission time
+int tx_state = 0;                // Current state of the transmitter
+char tx_data = 0b01100001;       // Data to transmit (ASCII 'a')
+int tx_bit_counter = 0;          // Counter for transmitted bits
+unsigned long random_wait_time = 1000000; // Initial random wait time in microseconds
 
-int samp_num = 3;
-int rx_rec_data;
-
-char data_char = 'a';
-
-int index=0;
-int state_rx = IDLE ;
-int state_tx = IDLE;
-int result_data;
-
-
-
-void setup()
-{
-	//open_serial_port
-	Serial.begin(19200); 
-  
-	pinMode(Tx_Data_Line, OUTPUT); 
-	pinMode(Rx_Data_Line, INPUT); 
-	
-	///calculate delta_time depend on bit time
-	delta_time = BIT_wait_time / (samp_num + 2);
-    
-	random_wait_time = random(1000,7000);
-
-	digitalWrite(Tx_Data_Line, HIGH);
-
-	Rx_ref_time = millis();
-	Tx_ref_time = millis();
-	
-	Serial.println("hey");
-
-
+void setup() {
+  pinMode(TX_PIN, OUTPUT);
+  pinMode(RX_PIN, INPUT);
+  digitalWrite(TX_PIN, HIGH);    // Set line HIGH (idle state)
+  Serial.begin(19200);
+  randomSeed(analogRead(0));     // Seed random generator
+  Serial.println("Transceiver Ready");
 }
 
+// Transmitter function
+void transmit() {
+  unsigned long current_time = micros();
 
+  // Random wait time between frames
+  if (tx_state == 0 && current_time - tx_last_time >= random_wait_time) {
+    tx_bit_counter = 0;
+    tx_state = 1;
+    random_wait_time = random(1000000, 5000000); // Random delay: 1 to 5 seconds
+    //Serial.println("Transmitting...");
+  }
 
-void usart_tx(){
-	
-	current_time = millis(); 
-	static int counter_tx = 0;
-	static int parity_bit = 0;	
-	
-	if (state_tx == IDLE){
-    
-		//random wait time
-		if(current_time - Tx_ref_time >= random_wait_time){
-			
-			state_tx = START;
-			Tx_ref_time = millis();
-		
-		}
-	}
-	else if(current_time - Tx_ref_time >= BIT_wait_time){
-	
-		switch(state_tx){
-		  
-		  case(START):
-		  parity_bit = 0;
-		  counter_tx = 0;
-		  digitalWrite(Tx_Data_Line, LOW);
-		  
-		  state_tx = DATA;
-		  Tx_ref_time = millis();
-		  break;
-
-		  case(DATA):
-		  if (counter_tx < 8) { 
-		  
-			  int current_bit = bitRead(data_char , counter_tx);
-			  digitalWrite(Tx_Data_Line, current_bit);
-			  parity_bit ^= current_bit;                 // Calculate parity using XOR
-			  counter_tx++;
-			  Tx_ref_time = millis();
-
-		  }
-		  else {
-		     state_tx = PARITY;
-			 Tx_ref_time = millis();
-
-		  }
-		  break;
-		  
-		  
-		  case(PARITY):		  
-		  digitalWrite(Tx_Data_Line, parity_bit ^ 1);
-          state_tx = STOP;
-		  Tx_ref_time = millis();
-		  break;
-		
-		  case(STOP):
-		  digitalWrite(Tx_Data_Line, HIGH);
-		  state_tx = IDLE;
-		  Tx_ref_time = millis();
-		  break;
-
-		
-		
-	}
-}
-
-}
-
-
-int wrapper_sample_data(int &index) {
-	
-	static int counter = 0;          // track number of calls
-	static int sample = 0; 
-	
-	current_time = millis();
-
-
-	if (counter == 0) {
-    	counter = index; 	// Set counter to match the initial value of index
-	}
-	
-  	
-
-	if(current_time - Rx_ref_time >= delta_time){
-	    
-
-		int bit = digitalRead(Rx_Data_Line);
-		sample = (sample << 1) | bit;  // Shift left and add the new bit// could cause problems with real arduino
-      	counter++;
-
-	}
-	
-		if (counter == 5){             // Check if 5 samples have been collected
-		
-		  
-          int result = sample & 0b1110;
-		  Serial.println("sampled five times");
-          Serial.println(result);
-		  if(result == 14){
-			result=1;
-		  } else if (result == 0){
-			result=0;
-		  }
-		  else{
-			result= 2;
-		  }
-		  
-		  counter = 0;                  // Reset counter for the next cycle
-		  sample = 0;  
-
-		  Rx_ref_time = millis(); // Reset timing
-		  return result;                
-		
-		}
-	
-	Rx_ref_time = millis(); // Reset timing
-	return -1;
-
-}
-
-
-void usart_rx(){
-	
-	static int counter_rx = 0;
-	static int data_parity_bit = 0;	
-    static int rx_parity_bit = 0;	
-    static int rx_data = 0;
-
-	switch (state_rx) {
-    
-	case IDLE:
-      rx_rec_data = digitalRead(Rx_Data_Line);
-      if (rx_rec_data == 0) {
-        state_rx = START;
-      }
-      break;
-
-    case START:
-	  // Sample 4 times
-      Serial.println("Start");
-
-      result_data = wrapper_sample_data(index=1);
-      if (result_data == 0) {
-        state_rx = DATA;
-      }
-      break;
-
-    
-	case DATA:
-	  
-	  //Serial.print("DATA ");
-	  result_data = wrapper_sample_data(index=0);
-
-        if (result_data != -1) {
-            int rec_bit = result_data;
-			data_parity_bit ^= rec_bit;                 // Calculate parity using XOR
-
-          	//Serial.println("rec");
-		    //Serial.println(rec_bit);
-
-          	rx_data |= (rec_bit << counter_rx);
-            counter_rx++;
-            if (counter_rx == 7) {
-                state_rx = PARITY;
-                counter_rx = 0; 
-            }
-        }
-		break;
-
-    
-	case PARITY:
-      	 
-      	Serial.print("PATITY ");
-
-        result_data = wrapper_sample_data(index=0);
-        if (result_data != -1) {
-            rx_parity_bit = result_data;
-            state_rx = STOP;
-
-        
-        }
-
+  if (tx_state > 0 && current_time - tx_last_time >= BIT_WAIT_TIME) {
+    switch (tx_state) {
+      case 1:
+        digitalWrite(TX_PIN, LOW); // Start bit
+        //Serial.println("Start bit sent");
+        tx_state = 2;
         break;
 
-    case STOP:
-
-        result_data = wrapper_sample_data(index=0);
-        //Serial.println("ending bit");
-        //Serial.println(result_data);
-
-        if (result_data != -1) {
-            int end_bit = result_data;
-            if (end_bit == 1) {
-                
-				if (rx_parity_bit == data_parity_bit){
-				
-                  	//Serial.println("Success");
-					//Serial.println((char)rx_data);
-
-				}
-				
-				//Serial.println("END");
-              	//Serial.flush();
-
-				
-
-            }
-          
-          //Serial.println("ENDING");
-
-          state_rx = IDLE;
-          rx_data = 0; 
-          rx_parity_bit = 0;
-          data_parity_bit = 0;
-        }
+      case 2:
+        digitalWrite(TX_PIN, (tx_data >> tx_bit_counter) & 1); // Send data bits
+        //Serial.print("Data bit sent: ");
+        //Serial.println((tx_data >> tx_bit_counter) & 1);
+        tx_bit_counter++;
+        if (tx_bit_counter >= 8) tx_state = 3;
         break;
 
-
+      case 3:
+        digitalWrite(TX_PIN, HIGH); // Stop bit
+        //Serial.println("Stop bit sent");
+        tx_state = 0; // Back to idle
+        break;
+    }
+    tx_last_time = current_time;
+  }
 }
-  
-}	
-		
 
-	
-void test_rx(){
-	
-	static int test_bit=0;
-	test_bit =wrapper_sample_data(index = 0);
-	
-  	//Serial.print("samplingthedatadata");
-	Serial.flush();
-	if (test_bit != -1){
-		
-	    Serial.println(" ");
-        Serial.print("recievied bit ");
-	    Serial.println(test_bit);
+// Receiver function
+void receive() {
+  unsigned long current_time = micros();
+  static int sample_counter = 0;
+  static int sampled_value = 0;
 
-	}
-	
+  if (rx_state == IDLE) {
+    if (digitalRead(RX_PIN) == LOW) {  // Detect start bit
+      rx_state = START;
+      rx_last_time = current_time;
+      sample_counter = 0;
+      sampled_value = 0;
+      //Serial.println("Start bit detected");
+    }
+  } else if (current_time - rx_last_time >= DELTA_TIME) {
+    sample_counter++;
+    sampled_value = (sampled_value << 1) | digitalRead(RX_PIN);
+
+    if (sample_counter == 5) {  // Process after sampling 5 times
+      int middle_bits = (sampled_value & 0b1110) >> 1; // Extract middle 3 bits
+      int ones_count = (middle_bits & 1) + ((middle_bits >> 1) & 1) + ((middle_bits >> 2) & 1);
+      int bit = (ones_count >= 2) ? 1 : 0; // Majority voting
+
+      sample_counter = 0; // Reset for the next bit
+      sampled_value = 0;
+
+      switch (rx_state) {
+        case START:
+          if (bit == 0) {  // Validate start bit
+            rx_state = DATA;
+            rx_bit_counter = 0;
+            rx_frame = 0;
+        //    Serial.println("Valid start bit");
+          } else {
+            rx_state = IDLE;  // Reset on invalid start bit
+            Serial.println("False start detected");
+          }
+          break;
+
+        case DATA:
+          rx_frame |= (bit << rx_bit_counter); // Store received bit
+          rx_bit_counter++;
+          //Serial.print("Received Bit: ");
+          //Serial.println(bit);
+          if (rx_bit_counter == 8) {  // After 8 bits, move to STOP state
+            rx_state = STOP;
+          }
+          break;
+
+        case STOP:
+          if (bit == 1) {  // Validate stop bit
+            Serial.print("Received Frame: ");
+            Serial.println(rx_frame, BIN);
+            Serial.print("Received Character: ");
+            Serial.println((char)rx_frame);
+          } else {
+            Serial.println("Stop bit error detected");
+          }
+          rx_state = IDLE;  // Reset after processing frame
+          break;
+      }
+    }
+    rx_last_time = current_time;
+  }
 }
 
 void loop() {
-	
-	
-	test_rx();
-	//usart_rx();
-   	usart_tx();
- 
-  
-  
+  transmit();
+  receive();
 }
-
-
-
-
-
 
