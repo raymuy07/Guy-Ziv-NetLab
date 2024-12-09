be59cbbb9dbfecb646c547fee715014ab65ea52c
LAPTOP-LFJH19DP\zivza <zivzaidman1@gmail.com>
2024-12-09 15:53:08 +0200

    test files

---
+++ Lab4-5/RX_test_lab4/RX_test_lab4.ino
@@ -0,0 +1,11 @@
+#include "EthernetLab.h"
+void setup() {
+  // put your setup code here, to run once:
+  setAddress (RX,10);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  testRX();
+}
