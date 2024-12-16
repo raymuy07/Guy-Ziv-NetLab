8ee27714594659a1eaf32c108019bbe58e5fdc5a
LAPTOP-LFJH19DP\zivza <zivzaidman1@gmail.com>
2024-12-16 18:33:31 +0200

    should send ack

--- Lab4-5/lab4_5_code_208279620_208660365_RX/lab4_5_code_208279620_208660365_RX.ino
+++ Lab4-5/lab4_5_code_208279620_208660365_RX/lab4_5_code_208279620_208660365_RX.ino
@@ -7,13 +7,18 @@ uint8_t destination_address;
 uint8_t source_address;
 uint8_t frame_type;
 uint8_t payload_length;
-
-int8_t raw_data[24];      // Buffer for raw data
-char payload_data[16];     // Buffer for payload data
+// we know the packet is with fixed size.
+uint8_t receive_buffer[25];  // 24 bytes: 4 header + 16 payload + 4 CRC
+char payload_data[17];     // Buffer for payload data
+char packets_buffer[250];
 unsigned long received_crc;  // Received CRC
 unsigned long calculated_crc; // Calculated CRC
-
-
+uint8_t send_buffer[25];  // 4 header + 17 payload + 4 CRC
+uint8_t ack_destination_address = 0x1A;
+uint8_t ack_source_address = 0x0A; 
+uint8_t ack_frame_type = 0;
+uint8_t ack_payload_length = 0;
+int ack_Sn=0;
 
 void setup() {
 	
@@ -27,10 +32,10 @@ void setup() {
 void extract_raw_data(uint8_t *raw_data){
 
 
-	  destination_address = raw_data[0];
+	destination_address = raw_data[0];
     source_address = raw_data[1];
     frame_type = raw_data[2];
-    payload_length = raw_data[3];
+    payload_length = raw_data[3];//SHOULD BE 17
 
     // Copy payload and ensure null-termination
     memset(payload_data, 0, sizeof(payload_data));  // Clear payload buffer
@@ -48,9 +53,32 @@ void extract_raw_data(uint8_t *raw_data){
 	// Verify CRC
     if (calculated_crc == received_crc) {
         Serial.println("CRC Verification: SUCCESS");
+		
     } else {
         Serial.println("CRC Verification: FAILED");
     }
+	
+}
+
+void build_ack_packet(){
+	
+    
+    
+	memset(send_buffer, 0, sizeof(send_buffer));  // Clear buffer
+	
+	send_buffer[0] = ack_destination_address;                   // Destination address
+    send_buffer[1] = ack_source_address;                        // Source address
+    send_buffer[2] = ack_frame_type;                        	// Frame type (0)
+    send_buffer[3] = ack_payload_length;                        // Payload length (fixed 16 bytes)
+
+    memcpy(&send_buffer[4], ack_Sn, payload_length);     
+	memcpy(&send_buffer[5], 0, payload_length-1);
+	
+	unsigned long crc = calculateCRC(send_buffer[0], 20);
+	memcpy(&send_buffer[4 + payload_length], &crc, 4);     // copy CRC 
+	
+	
+	
 }
 
 
@@ -69,30 +97,27 @@ void print_data(){
 }
 
 void loop() {
-    
-	
-	// we know the packet is with fixed size.
-	uint8_t receive_buffer[24];  // 24 bytes: 4 header + 16 payload + 4 CRC
+
     
     // Check if a packet has been received
 	if (readPackage(receive_buffer, sizeof(receive_buffer))) {
-
-
-//
+	
     Serial.println("Raw Data Received:");
-        
-        for (int i = 0; i < 24; i++) {
+        for (int i = 0; i < 25; i++) {
             Serial.print(receive_buffer[i], HEX);
             Serial.print(" ");
         }
-        Serial.println();
-		//
-
-        extract_raw_data(receive_buffer); 
-    
-	} else {
-        delay(100);
+	extract_raw_data(receive_buffer);
+	if (calculated_crc == received_crc) {
+        Serial.println("CRC Verification: SUCCESS");
+		ack_Sn = ((payload_data[0]&0x01)^1);
+		build_ack_packet();//should br ack with new sn
+    } else {
+        Serial.println("CRC Verification: FAILED");
+		build_ack_packet;//should be ack with earlier sn
     }
+    
+	}
 	
 }
 
