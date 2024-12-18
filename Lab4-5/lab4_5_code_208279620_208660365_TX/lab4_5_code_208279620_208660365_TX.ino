3985c50e66d5dcc8b2cefdf267e3b643f0e5eebf
DESKTOP-D7MOSLV\Guy <guylei@post.bgu.ac.il>
2024-12-18 11:03:38 +0100

    Fixed code for stop and wait

--- Lab4-5/lab4_5_code_208279620_208660365_TX/lab4_5_code_208279620_208660365_TX.ino
+++ Lab4-5/lab4_5_code_208279620_208660365_TX/lab4_5_code_208279620_208660365_TX.ino
@@ -7,11 +7,19 @@
 uint8_t frame_type = 0x00;
 uint8_t source_address = 0x1A ;
 uint8_t destination_address = 0x0A ;
-uint8_t ack_sn_data = 0x00;
+
+
+uint8_t sn_index = 0;                        // Sequence number of frame
+uint8_t rn_index = 0;                        // Received ACK number
+uint8_t ack_sn_data = 0x00;                  // Field combining ACK/DATA and SN
+
 
 char payload_data[]= "Leiba & Zaidman";
 uint8_t payload_length = strlen(payload_data);
 
+
+unsigned long ref_time, current_time;
+const unsigned long time_out = 8000;         // timeout duration in ms
 
 uint8_t send_buffer[25];  // 4 header + 16 payload + 4 CRC
 uint8_t ack_buffer[25];
@@ -22,7 +30,7 @@ void setup() {
   
   setAddress(TX, 10);   
   Serial.println("Transmitter initialized.");
-  
+  ref_time = millis();
 
 }
 
@@ -31,10 +39,9 @@ void setup() {
 void build_packet(){
 	
     
-    
 	memset(send_buffer, 0, sizeof(send_buffer));  // Clear buffer
 	
-	  send_buffer[0] = destination_address;                   // Destination address
+	send_buffer[0] = destination_address;                   // Destination address
     send_buffer[1] = source_address;                        // Source address
     send_buffer[2] = frame_type;                        	// Frame type (0)
     send_buffer[3] = payload_length;                        // Payload length (fixed 16 bytes)
@@ -42,8 +49,7 @@ void build_packet(){
 	
 	///Here I need to shift the size to the left
 
-	ack_sn_data = payload_length <<;
-	ack_sn_data = ack_sn_data || sn_index;
+	ack_sn_data = (payload_length << 1) | (sn_index & 0x01);
 	send_buffer[4] = ack_sn_data;	
 	
 	
@@ -53,104 +59,63 @@ void build_packet(){
 	unsigned long crc = calculateCRC(send_buffer[0], 20);
 	memcpy(&send_buffer[5 + payload_length], &crc, 4);     // copy CRC 
 	
-  Serial.println("Serialized Buffer on TX:");
-for (int i = 0; i < 24; i++) {
+	
+	/* DEBUG
+	Serial.println("Serialized Buffer on TX:");
+	for (int i = 0; i < 24; i++) {
     Serial.print(send_buffer[i], HEX);
-    Serial.print(" ");
-}
-Serial.println();
-
-	
-	
+    Serial.println(" ");
+	}	
+	*/
 	
 }
 
-void is_ack(){
-	
-	
-	recieved_ack = readPackage(ack_buffer, sizeof(ack_buffer));
 
-	if (recieved_ack){ 
+void is_time_out() {
 	
-	//check if the ack is for good packet or for bad packet.
-	//if the sn_index is different then the rn_index meaning that 
-	//the reciever got a good frame and we need to continue.
-	
-	sn_index = ack_buffer[4];
+    current_time = millis();
 
+    // timeout check
+    
+	if (current_time - ref_time >= time_out) {
+        Serial.println("Timeout! Resending frame...");
+        sendPackage(send_buffer, sizeof(send_buffer));
+        ref_time = millis();
+    }
+}
+
+
+
+void is_ack() {
 	
-	if (sn_index != rn_index){
-			
-			sn_index = rn_index;
-			//packet_index ++;
-	}
+	
+    bool received_ack = readPackage(ack_buffer, sizeof(ack_buffer));
+    
+	if (received_ack) {
+    
+		uint8_t ack_sn = ack_buffer[4] & 0x01;  // Extract SN bit from ACK
+
+        if (ack_sn == sn_index) {  // Correct ACK received
 		
-	//build the next packet or the same packet
-	
-	//build_packet(packet_index); 
-	build_packet()
-	ref_time = current_time;
-	sendPackage(send_buffer, sizeof(send_buffer));
-
-	
-	}
-	
-	
-	
+            Serial.println("Correct ACK received. Sending next frame...");
+            sn_index ^= 0x01;      // Flip SN (0 -> 1, 1 -> 0)
+            
+			//it is the same packet but in case there will be more data...
+			build_packet();        // build next packet
+            
+			sendPackage(send_buffer, sizeof(send_buffer));
+            ref_time = millis();   // Reset timer
+        }
+    }
 }
 
 
 
-//to be moved
-unsigned long ref_time,current_time;
+
 
 void loop() {
 	
-	current_time = millis();
-
-	is_ack();
-	
-	
-	
-	int packet_index = 0;
-
-	
-	
-	
-	
-	
-	// if for timeout and sending the same data
-
-	if (current_time - ref_time >= time_out) {
-		
-		
-		
-
-		build_packet(packet_index); 
-
-		sendPackage(send_buffer, sizeof(send_buffer));
-		rn_index = sn_index;
-		
-		ref_time = current_time;
-		
-		recieve_ack = 0;
-		
-	}
-	
-	
-	
-	current_time = millis();
-	
-  
-  if (sendPackage(send_buffer, sizeof(send_buffer))) {
-        
-		Serial.println("Frame sent successfully!");
-		build_packet();
-    } 
-	
-	else {
-        Serial.println("Line busy, retrying...");
-		delay(1000);
-    }
+	is_time_out();  
+    is_ack();    
 
 }
